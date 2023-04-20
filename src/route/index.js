ecfca760c2a508b9396023955e49f4307b1da670
MBA <mba@MacBook-Air-MBA.local>
2023-04-20 14:08:15 +0200

    done 4 lessons

--- src/route/index.js
+++ src/route/index.js
@@ -5,6 +5,37 @@ const router = express.Router()
 
 // ================================================================
 
+var header = {
+  name: {
+    firstname: 'Ivan',
+    lastname: 'Ivanov',
+  },
+  position: 'Junior Fullstack JS Developer',
+  salary: '600$ в місяц',
+  address: 'str. Metrologichna 23, Kiyv, Ukraine',
+}
+
+var footer = {
+  social: {
+    email: {
+      text: 'Ivanov@mail.com',
+      href: 'mailto:Ivanov@mail.com',
+    },
+
+    phone: {
+      text: '+380670000123',
+      href: 'tel:+380670000123',
+    },
+
+    linkedin: {
+      text: 'LinkedIn',
+      href: 'https://www.linkedin.com/in/dmytro-test',
+    },
+  },
+}
+
+// ================================================================
+
 // router.get Створює нам один ентпоїнт
 
 //           ↙ тут вводимо шлях (PATH) до сторінки
@@ -23,10 +54,222 @@ router.get('/summary', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('summary', {
     // ↙ сюди вводимо JSON дані
+    page: {
+      title: 'Resume | Summary',
+    },
+
+    header,
+
+    main: {
+      summary: {
+        title: 'Summary',
+        text: `Open-minded for new technologies, with 1 years of experience in development. Whenever I start to
+        work on a new project I learn the domain and try to understand the idea of the project. Good team
+        player, every colleague is a friend to me.`,
+      },
+
+      experience: {
+        title: 'Other experience',
+        text: `Pet project for parsing sport betting data from different platforms ( odds ) and sport statistics (
+          tournament position, goals etc), analyzing by simple mathematics models and preparing probability
+          for such events like: money line - first win / draw / second win, totals etc.`,
+      },
+    },
+
+    footer,
   })
 })
 
 // ================================================================
+// ================================================================
+
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/skills', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('skills', {
+    // ↙ сюди вводимо JSON дані
+    page: {
+      title: 'Resume | Skills',
+    },
+
+    header,
+
+    main: {
+      skills: [
+        {
+          name: 'HTML',
+          point: 10,
+          isTop: true,
+        },
+        {
+          name: 'Handlebars',
+          point: 10,
+          isTop: true,
+        },
+        {
+          name: 'VS Code',
+          point: 8,
+          isTop: false,
+        },
+        {
+          name: 'GIT',
+          point: 10,
+        },
+        {
+          name: 'Terminal',
+          point: 10,
+        },
+        {
+          name: 'NPM',
+          point: 10,
+        },
+        {
+          name: 'React.js',
+          point: 0,
+        },
+        {
+          name: 'PHP',
+          point: null,
+        },
+      ],
+      hobbies: [
+        {
+          name: 'Futbol',
+          isMain: true,
+        },
+        {
+          name: 'Billiards',
+          isMain: true,
+        },
+        {
+          name: 'Tennis',
+          isMain: false,
+        },
+      ],
+    },
+
+    footer,
+  })
+})
+//============================================================
+//============================================================
+
+router.get('/education', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('education', {
+    // ↙ сюди вводимо JSON дані
+    page: {
+      title: 'Resume | Education',
+    },
+
+    header,
+
+    main: {
+      educations: [
+        {
+          name: 'Національний Авіаційний Університет',
+          isEnd: true,
+        },
+        {
+          name: 'Національна Юридична Академія',
+          isEnd: true,
+        },
+        {
+          name: 'Національний Університет нафти та газу',
+          isEnd: true,
+        },
+        {
+          name: 'IT BRAINS',
+          isEnd: false,
+        },
+      ],
+      certificates: [
+        {
+          name: 'Certificat NAU',
+          id: 'UA 555777',
+        },
+        {
+          name: 'Certificat NLA',
+          id: 'UA 123765',
+        },
+        {
+          name: 'Certificat NUN',
+          id: 'UA 777555',
+        },
+      ],
+    },
+
+    footer,
+  })
+})
+
+//============================================================
+//============================================================
+
+router.get('/work', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('work', {
+    // ↙ сюди вводимо JSON дані
+
+    layout: 'big',
+
+    page: {
+      title: 'Resume | Work',
+    },
+
+    header,
+
+    main: {
+      works: [
+        {
+          position: 'Junior Fullstack Developer',
+          company: {
+            name: 'IT Brains',
+            url: 'http://it-brains.com.ua/',
+          },
+          duration: {
+            from: '10.10.2022',
+            to: null,
+          },
+
+          projectAmount: 3,
+
+          projects: [
+            {
+              name: 'Resume',
+              url: 'http://resume.com.ua/',
+              about:
+                'Airbnb competitor. High-load application for searching apartments',
+              stackAmount: 5,
+              stacks: [
+                {
+                  name: 'React.js',
+                },
+                {
+                  name: 'HTML / CSS',
+                },
+                {
+                  name: 'Node.js',
+                },
+              ],
+              awardAmount: 7,
+              awards: [
+                {
+                  name: 'Backgroud verification - is a feature that provides users to prove that they are real persons.',
+                },
+                {
+                  name: 'Preparing SEO optimized pages. The automated process of getting data for the app from documents',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+
+    footer,
+  })
+})
 
 // Підключаємо роутер до бек-енду
 module.exports = router
